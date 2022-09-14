d453650d2dbb3b547f6693455fd75dad709245b4
bushijie <bushijie@users.noreply.github.com>
2022-09-14 15:57:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oqhcU6R3bFuDiB7Cl0ZmdzIOfs20",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "nng0IEaNK2epr_JFFq1LY0aRlLcbzPaHJVn_V4wERM8",
+      useTemplateId: "QFFIoPgZoYQ3CL8WqGFt-K_g8rRVxMT2We76a4r3iXc",
       // 所在省份
       province: "湖北",
       // 所在城市
